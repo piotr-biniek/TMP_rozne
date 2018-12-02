3f37ffe361f3cf2e6bc216d7b0d1fc6f6d460c8d
Piotr Biniek <piotr_biniek@wp.pl>
2018-12-02 23:17:00 +0100

    "Produkcja" MyArrayList - do dodoania kilka metod potem przetestowanie!

--- AlgorytmyIZadaniaRozne/AlgorytmyProste/src/main/java/pl/biniek/kolekcje/MyArrayList.java
+++ AlgorytmyIZadaniaRozne/AlgorytmyProste/src/main/java/pl/biniek/kolekcje/MyArrayList.java
@@ -1,52 +1,115 @@
 /*
- * do zrobienia implementacja listy tablicowej
+ * moja implementacja Arraylisty, tak cwiczebnie i poznawczo!
+ *todo do dokonczenia
  */
 package pl.biniek.kolekcje;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Objects;
 
 /**
  *
  * @author piotr
  */
-public class MyArrayList <T>implements List{//todo naniedziele
+public class MyArrayList<T> implements List<T> {//todo naniedziele
+
+    Object[] elements;
+    int maxSize;
+    int actualSize;
+    
+
+    /**
+     * constructs list of expected size
+     */
+    public MyArrayList(int initialCapacity) {
+        elements = new Object[initialCapacity];
+        actualSize = 0;
+        maxSize = initialCapacity;
+
+    }
+
+    /**
+     * constructs default size list of size 50
+     */
+    public MyArrayList() {
+
+        this(50);
+        maxSize = 50;
+
+    }
+
+    public MyArrayList(Collection<? extends T> c) {
+        for (T t : c) {
+            elements[actualSize++]=c;
+            
+        }
+    }
+
+    private void increase() {
+      Object[] tmpElements=new Object[(actualSize*2)];
+        for (int i = 0; i < actualSize; i++) {
+            tmpElements[i]=elements[i];
+        }
+        elements=tmpElements;
+        maxSize=actualSize*2;
+            
+            
+        }
+    
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return actualSize;
     }
 
     @Override
     public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return size() == 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return this.indexOf(o) > 0;
+
     }
 
     @Override
     public Iterator iterator() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return new MyIterator();
+
     }
 
     @Override
     public Object[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Object[] newArray = new Object[actualSize];
+        for (int i = 0; i < actualSize; i++) {
+            newArray[i] = elements[i];
+
+        }
+        return newArray;
     }
 
     @Override
-    public Object[] toArray(Object[] a) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+
+    public <T> T[] toArray(T[] a) {
+
+        return (T[]) Arrays.copyOf(elements, size(), a.getClass());
+
     }
 
     @Override
-    public boolean add(Object e) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean add(T e) {
+        if (actualSize == maxSize) {
+            increase();
+        }
+
+        elements[actualSize++] = e;
+        return true;
+
     }
 
     @Override
@@ -81,12 +144,17 @@ public class MyArrayList <T>implements List{//todo naniedziele
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        for (int i = 0; i < actualSize; i++) {
+            elements[i]=null;
+            
+        }
+        actualSize=0;
+        
     }
 
     @Override
-    public Object get(int index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public T get(int index) {
+        return (T) elements[index];
     }
 
     @Override
@@ -100,18 +168,35 @@ public class MyArrayList <T>implements List{//todo naniedziele
     }
 
     @Override
-    public Object remove(int index) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public T remove(int index) {
+        T tmp = (T) elements[index];
+        for (int i = index; i < actualSize-1; i++) {
+            elements[i]=elements[i+1];
+            
+        }
+        return tmp;
     }
 
     @Override
     public int indexOf(Object o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
+        for (int i = 0; i < actualSize;i ++) {
+            if(Objects.equals(elements[i], o)) return i;
+            
+            
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+          
+        for (int i =( actualSize-1); i >=0;i --) {
+            if(Objects.equals(elements[i], o)) return i;
+            
+            
+        }
+        return -1;
     }
 
     @Override
@@ -125,10 +210,28 @@ public class MyArrayList <T>implements List{//todo naniedziele
     }
 
     @Override
-    public List subList(int fromIndex, int toIndex) {
+    public List<T> subList(int fromIndex, int toIndex) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
-    
-    
+
+    private class MyIterator<E> implements Iterator {
+
+        int index = 0;
+
+        public MyIterator() {
+
+        }
+
+        @Override
+        public boolean hasNext() {
+            return index != size();
+        }
+
+        @Override
+        public Object next() {
+            return get(++index);
+            // throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
 }
