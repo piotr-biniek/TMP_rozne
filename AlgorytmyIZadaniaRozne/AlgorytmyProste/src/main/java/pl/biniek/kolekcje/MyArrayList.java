b18222230d265930ff49f38550935b61a6c640e5
Piotr Biniek <piotr_biniek@wp.pl>
2018-12-03 17:16:10 +0100

    Dodano troche kodu do MyArrayList -  do wsępnego  przetestowania!

--- AlgorytmyIZadaniaRozne/AlgorytmyProste/src/main/java/pl/biniek/kolekcje/MyArrayList.java
+++ AlgorytmyIZadaniaRozne/AlgorytmyProste/src/main/java/pl/biniek/kolekcje/MyArrayList.java
@@ -18,9 +18,8 @@ import java.util.Objects;
 public class MyArrayList<T> implements List<T> {//todo naniedziele
 
     Object[] elements;
-    int maxSize;
+    // int elements.length;
     int actualSize;
-    
 
     /**
      * constructs list of expected size
@@ -28,7 +27,7 @@ public class MyArrayList<T> implements List<T> {//todo naniedziele
     public MyArrayList(int initialCapacity) {
         elements = new Object[initialCapacity];
         actualSize = 0;
-        maxSize = initialCapacity;
+        // elements.length = initialCapacity;
 
     }
 
@@ -38,28 +37,25 @@ public class MyArrayList<T> implements List<T> {//todo naniedziele
     public MyArrayList() {
 
         this(50);
-        maxSize = 50;
+        // elements.length = 50;
 
     }
 
     public MyArrayList(Collection<? extends T> c) {
         for (T t : c) {
-            elements[actualSize++]=c;
-            
+            elements[actualSize++] = c;
+
         }
     }
 
     private void increase() {
-      Object[] tmpElements=new Object[(actualSize*2)];
+        Object[] tmpElements = new Object[(elements.length * 2)];
         for (int i = 0; i < actualSize; i++) {
-            tmpElements[i]=elements[i];
+            tmpElements[i] = elements[i];
         }
-        elements=tmpElements;
-        maxSize=actualSize*2;
-            
-            
-        }
-    
+        elements = tmpElements;
+
+    }
 
     @Override
     public int size() {
@@ -73,7 +69,7 @@ public class MyArrayList<T> implements List<T> {//todo naniedziele
 
     @Override
     public boolean contains(Object o) {
-        return this.indexOf(o) > 0;
+        return this.indexOf(o) >= 0;
 
     }
 
@@ -103,7 +99,7 @@ public class MyArrayList<T> implements List<T> {//todo naniedziele
 
     @Override
     public boolean add(T e) {
-        if (actualSize == maxSize) {
+        if (actualSize == elements.length) {
             increase();
         }
 
@@ -114,7 +110,18 @@ public class MyArrayList<T> implements List<T> {//todo naniedziele
 
     @Override
     public boolean remove(Object o) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int forRemove = indexOf(o);
+        if (forRemove >= 0) {
+            elements[forRemove] = null;
+
+            int objectsToMove = actualSize - forRemove - 1;
+            if (objectsToMove > 0) {
+                System.arraycopy(elements, forRemove + 1, elements, forRemove, objectsToMove);
+            }
+            return true;
+
+        }
+        return false;
     }
 
     @Override
@@ -123,33 +130,63 @@ public class MyArrayList<T> implements List<T> {//todo naniedziele
     }
 
     @Override
-    public boolean addAll(Collection c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean addAll(Collection<? extends T> c) { //chyba tak powinno być ograniczenie od góry
+        //żeby kolekcja nie pozwalała dodawać dowolej kolekcji 
+        // klasy  rozszerzającej klase object czyli każdej
+        c.forEach((object) -> {
+            add((T) object);
+        });
+        return true;
     }
 
     @Override
-    public boolean addAll(int index, Collection c) {
+    public boolean addAll(int index, Collection<? extends T> c) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
     public boolean removeAll(Collection c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean isAnythingRemoved = false;
+        for (Object object : c) {
+            if (remove((T)object)) {
+                isAnythingRemoved = true;
+            }
+        }
+        return isAnythingRemoved;
     }
 
     @Override
-    public boolean retainAll(Collection c) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public boolean retainAll(Collection<?> c) {//z interfacu lista!! wynika <?>
+
+        int oryginalSize = this.size();
+
+        Object[] newElements = new Object[actualSize];
+        int tmpActualSize = 0;
+        int tmpIndex = 0;
+
+        // int i=0;
+        for (Object object : c) {
+            if ((tmpIndex = indexOf(object)) > -1) {
+                newElements[tmpActualSize++] = object;
+
+            }
+
+        }
+        elements = newElements;
+        actualSize = tmpActualSize;
+
+        return !(oryginalSize == tmpActualSize);
+
     }
 
     @Override
     public void clear() {
         for (int i = 0; i < actualSize; i++) {
-            elements[i]=null;
-            
+            elements[i] = null;
+
         }
-        actualSize=0;
-        
+        actualSize = 0;
+
     }
 
     @Override
@@ -170,31 +207,33 @@ public class MyArrayList<T> implements List<T> {//todo naniedziele
     @Override
     public T remove(int index) {
         T tmp = (T) elements[index];
-        for (int i = index; i < actualSize-1; i++) {
-            elements[i]=elements[i+1];
-            
+        for (int i = index; i < actualSize - 1; i++) {
+            elements[i] = elements[i + 1];
+
         }
         return tmp;
     }
 
     @Override
     public int indexOf(Object o) {
-       
-        for (int i = 0; i < actualSize;i ++) {
-            if(Objects.equals(elements[i], o)) return i;
-            
-            
+
+        for (int i = 0; i < actualSize; i++) {
+            if (Objects.equals(elements[i], o)) {
+                return i;
+            }
+
         }
         return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-          
-        for (int i =( actualSize-1); i >=0;i --) {
-            if(Objects.equals(elements[i], o)) return i;
-            
-            
+
+        for (int i = (actualSize - 1); i >= 0; i--) {
+            if (Objects.equals(elements[i], o)) {
+                return i;
+            }
+
         }
         return -1;
     }
